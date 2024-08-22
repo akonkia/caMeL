8021c6d2c9a25a55f604cdbddd0a0889c0f524ba
Agnieszka Konkolewska <68012357+akonkia@users.noreply.github.com>
2024-08-22 12:04:21 +0200

    Update license details

--- funcsFQual5.R
+++ funcsFQual5.R
@@ -1,3 +1,16 @@
+#caMeL - a chemometrics applied machine learning interface
+#Copyright (C) 2024  Agnieszka Konkolewska
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
 library(EMSC)
 
 cvPlot <- function(data_long, type = "RMSE", trait_vector, where = "bottom"){
